1d7082e836759bfb7590ae67ca49decc2c88191b
ttian99 <176553611@qq.com>
2018-11-05 18:02:41 +0800

    修改证书配置方式，增加请求时间输出

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
+const path = require('path');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const express = require('express')
+const express = require('express');
 const app = express()
 const config = require('./config/config.json');
 const allCrossDomain = require('./lib/middleware/allowCrossDomain');
+var favicon = require('serve-favicon');
+var compression = require('compression');
 
-// 跨域问题解决
-app.use(allCrossDomain);
-// 启用静态文件服务
-app.use(express.static(config.staticRoot));
-app.get('/', (req, res) => res.send('Hello World!'))
-
-// 证书 Certificate
-const privateKey = fs.readFileSync('./certificate/private.pem', 'utf8');
-const certificate = fs.readFileSync('./certificate/ca.cer', 'utf8');
+// 关卡数据
+const star_mission_data = require('./public/star-mission-data.json');
+// https证书
+const privateKey = fs.readFileSync(path.join(__dirname, config.key), 'utf8');
+const certificate = fs.readFileSync(path.join(__dirname, config.cer), 'utf8');
 const svrOpts = {
     key: privateKey,
     cert: certificate
 }
-// 服务器创建
+// 启动服务器
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(svrOpts, app);
-httpServer.listen(config.httpPort, () => console.log(`HTTP Server running on port ${config.httpPort}!, url: http://127.0.0.1:${config.httpPort} `))
-httpsServer.listen(config.httpsPort, () => console.log(`HTTPS Server running on port ${config.httpsPort}!, url: https://127.0.0.1:${config.httpsPort} `))
+httpServer.listen(config.httpPort, () => console.log(`HTTP Server running on port ${config.httpPort}!`))
+httpsServer.listen(config.httpsPort, () => console.log(`HTTPS Server running on port ${config.httpsPort}!`))
+
+/** 中间件 */
+app.use(allCrossDomain); // 允许跨域访问
+app.use(favicon('res/favicon.ico')); 
+app.use(compression()); // 开启Gzip压缩
+app.use(function (req, res, next) {  // 记录请求时间
+    const start = req.query.startTime;
+    const end = new Date().getTime() / 1000;
+    const del = end - start;
+    console.log(`${http.protocol} : ${req.url} , star=${starTime}, end=${endTime}, del=${del}`);
+    next();
+});
+
+// 测试路径
+app.get('/test', (req, res) => res.send('恭喜你，找到了后门！'));
+// 关卡数据请求地址
+app.get('/star-mission-data.json', (req, res) => {
+    res.send(JSON.stringify(star_mission_data));
+})
